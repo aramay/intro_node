7bd1e2ac0247978c73d7c2b16aedac2e8e4850d7
“kasper341” <kasper_341@hotmail.com>
2016-11-22 11:20:33 -0800

    add 'nextTick' function example

--- node_process_object.js
+++ node_process_object.js
@@ -1,5 +1,20 @@
 // the process object, part of then node.js global namespace. The process object is extremely useful for identifying information about the runtime environment of your node app such as the version of node, the arguments passed to the node executable, the current working directory, and the nextTick function.
 
-function functionName() {
+// var logger = function (d) {
+//     process.stdout.write(d + '\n');
+// };
+//
+// logger("hello");
 
-}
+
+// process.argv.forEach(function (val, index) {
+//     console.log(index +" : "+ val );
+// });
+
+console.log("start");
+
+process.nextTick(function () {
+    console.log("nextTick callback");
+});
+
+console.log("scheduled");
