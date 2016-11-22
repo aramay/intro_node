eed320555af63a551c731a30871cc460e32cbfd7
“kasper341” <kasper_341@hotmail.com>
2016-11-22 09:12:08 -0800

    add node process object

---
+++ node_process_object.js
@@ -0,0 +1,5 @@
+// the process object, part of then node.js global namespace. The process object is extremely useful for identifying information about the runtime environment of your node app such as the version of node, the arguments passed to the node executable, the current working directory, and the nextTick function.
+
+function functionName() {
+
+}
