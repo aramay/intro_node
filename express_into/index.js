53563b78511798554e970dc148d071e15d991ce0
“kasper341” <kasper_341@hotmail.com>
2016-11-26 09:42:29 -0800

    add callback to log server mesges

--- express_into/index.js
+++ express_into/index.js
@@ -1,1 +1,17 @@
-var express = require('express')
+var express = require('express');
+
+var app = express();
+
+//call this function for http GET request for this path '/'
+app.get('/', function (req, res) {
+
+    res.send("hello world");
+
+});
+
+// var server = app.listen(3000);
+//pass this function a call back added info logging
+
+var server = app.listen(3000, function () {
+    console.log("Server running at localhost:" + server.address().port);
+});
