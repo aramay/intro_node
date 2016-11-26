f318cf99e2cc9afee28facae87ad0b6e8f27b298
“kasper341” <kasper_341@hotmail.com>
2016-11-26 11:00:20 -0800

    add 'fs,readFile' to read data of file 'users.json'

--- express_into/index.js
+++ express_into/index.js
@@ -1,12 +1,40 @@
 var express = require('express');
+var fs = require('fs');
+var _ = require('lodash');
 
 var app = express();
 
+
+var users = [];
+
+
+fs.readFile('users.json', 'utf8', function (err, data) {
+    if (err) {
+        throw err;
+    }else{
+        JSON.parse(data).forEach(function (user) {
+            user.name.full = _.startCase(user.name.first +" "+ user.name.last);
+
+            users.push(user);
+        });
+    }
+});
 //call this function for http GET request for this path '/'
 app.get('/', function (req, res) {
-    res.send("hello, world!");
+
+    var buffer = "";
+
+    users.forEach(function (user) {
+
+        buffer += '<a href="/' +user.username +'"> '+ user.name.full + ' </a><br>';
+        // '<a href="/"' +user.username+' user.name.full + '<br>';
+    });
+    // res.send(JSON.stringify(users));
+    res.send(JSON.stringify(buffer));
 
 });
+
+
 
 app.get('/yo', function (req, res) {
     res.send("yo !!");
