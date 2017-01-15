96949eb56496a1bd750facd098e9577494ee5bda
“kasper341” <kasper_341@hotmail.com>
2017-01-14 19:17:25 -0800

    implmented using 'utf8' char encoding method

--- learnyounode/first_io.js
+++ learnyounode/first_io.js
@@ -1,9 +1,11 @@
 var fs = require('fs')
 var fileName = process.argv[2]
+// var fileBuffer = fs.readFileSync(fileName)
 var fileBuffer = fs.readFileSync(fileName, 'utf8')
 
 
 
-var lines = fileBuffer.toString().split('\n').length - 1
+// var lines = fileBuffer.toString().split('\n').length - 1
+var lines = fileBuffer.split('\n').length - 1
 console.log(lines)
 
