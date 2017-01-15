eff21a9f94040d5ef6184637923ae1e1f9a7f038
“kasper341” <kasper_341@hotmail.com>
2017-01-14 19:16:11 -0800

    implmenented using 'toString' method

--- learnyounode/first_io.js
+++ learnyounode/first_io.js
@@ -1,9 +1,9 @@
 var fs = require('fs')
+var fileName = process.argv[2]
+var fileBuffer = fs.readFileSync(fileName, 'utf8')
 
-var buf = fs.readFileSync('./zork.txt')
 
-console.log(buf.toString().split())
 
-var line = buf.toString().split()
+var lines = fileBuffer.toString().split('\n').length - 1
+console.log(lines)
 
-while()
