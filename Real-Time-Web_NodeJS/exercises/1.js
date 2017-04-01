e696fdd1883267d70bdba517306ea0973efbede8
“kasper341” <kasper_341@hotmail.com>
2017-03-31 23:25:05 -0700

    implemented fs.readFileSync function

--- Real-Time-Web_NodeJS/exercises/1.js
+++ Real-Time-Web_NodeJS/exercises/1.js
@@ -4,17 +4,19 @@ function printHelp(){
    console.log("")
    console.log("usage:")
    console.log("--help        print this help")
-   console.log("--name        say hello to {NAME}")
+   console.log("--file={name}        read the file of name {NAME} and output its content")
 }
-var args = require('minimist')(process.argv.slice(2), {string: "name"})
+var args = require('minimist')(process.argv.slice(2), {string: "file"})
 
-if (args.help || !args.name) {
+if (args.help || !args.file) {
    printHelp()
    process.exit(1)
 }
 // var name = process.argv[2]
-var name = args.name
 
-console.log("hello " + name)
+var hello = require('./helloworld.js')
+
+var contents = hello.say(args.file)
+console.log(contents.toString())
 
 // process.stdout.write("stdout ->")
