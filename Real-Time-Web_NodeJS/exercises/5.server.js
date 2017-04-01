3c584b3ec5d2980d48326223811423f7da7680aa
“kasper341” <kasper_341@hotmail.com>
2017-04-01 11:31:57 -0700

    finish 'async' behaviour to web-server

--- Real-Time-Web_NodeJS/exercises/5.server.js
+++ Real-Time-Web_NodeJS/exercises/5.server.js
@@ -1,18 +1,31 @@
 
 function handleHttp (req, res) {
   if (req.method === 'GET') {
-     if (req.url === '/') {
+    if (req.url === '/') {
       //   console.log(req)
-        res.writeHead(200, {'Content-type': 'text/plain' })
-        res.end('Node server running' + Math.random(10))
-      }      else {
-        res.writeHead(403)
-        res.end('Forbidden')
-      }
-   }   else {
-     res.writeHead(403)
-     res.end('Forbidden')
-   }
+
+       ASQ(function (done) {
+        setTimeout(function () {
+          var num = Math.random()
+          done(num)
+        }, 1000)
+      })
+      .then(function (done, num) {
+        setTimeout(function () {
+          // res.end("async request" + )
+          done('async request ' + num)
+        }, 1000)
+      })
+      .val(function (mesg) {
+        res.end(mesg)
+      })
+     }// end if req.url
+
+     else{
+       res.writeHead(403)
+       res.end("bad request")
+     }
+  }
 }
 
 var port = 8080
@@ -20,3 +33,5 @@ var host = 'localhost'
 
 var http = require('http')
 var http_serv = http.createServer(handleHttp).listen(port, host)
+
+var ASQ = require('asynquence')
