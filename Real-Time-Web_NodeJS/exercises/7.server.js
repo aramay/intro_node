f83716585b64120852fb74dc31b3dd61f3aeda73
“kasper341” <kasper_341@hotmail.com>
2017-04-01 12:15:49 -0700

    add 'scoket' connection to server

--- Real-Time-Web_NodeJS/exercises/7.server.js
+++ Real-Time-Web_NodeJS/exercises/7.server.js
@@ -1,26 +1,26 @@
 function handleHTTP (req, res) {
   if (req.method == 'GET') {
     if (/^\/\d+(?=$|[\/?#])/.test(req.url)) {
-    req.addListener('end', function () {
-    req.url = req.url.replace(/^\/(\d+).*$/, '/$1.html')
-    static_files.serve(req, res)
-  })
-    req.resume()
-  }		else {
-    res.writeHead(403)
-    res.end()
+      req.addListener('end', function () {
+        req.url = req.url.replace(/^\/(\d+).*$/, '/$1.html')
+        static_files.serve(req, res)
+      })
+      req.resume()
+    }	 else {
+      res.writeHead(403)
+      res.end()
+    }
   }
-  }	else {
+  else {
     res.writeHead(403)
     res.end()
   }
 }
 
-var
-  http = require('http'),
+var http = require('http'),
   httpserv = http.createServer(handleHTTP),
 
-  port = 8006,
+  port = 8080,
   host = '127.0.0.1',
 
   ASQ = require('asynquence'),
@@ -30,3 +30,7 @@ var
 require('asynquence-contrib')
 
 httpserv.listen(port, host)
+
+var io = require('socket.io')
+// hijack existing http connection , and listen for socket.io connection
+io.listen(httpserv)
